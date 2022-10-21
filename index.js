2b6011cab9e3a9246adaa99000b2c96d1dfaa0a9
thegwanj <gwanjuchung@gmail.com>
2022-10-21 15:11:33 -0700

    Added in prompts for other employee roles

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 
 // Create an empty array list to store employee objects
+let employees = [];
 
 // Gather manager data
 
@@ -28,12 +29,6 @@ const inquirer = require('inquirer');
             message: `What is the team manager's office number?`,
             name: 'managerOffice',
             },
-            {
-            type: 'list',
-            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
-            message: `Which type of team member would you like to add?`,
-            name: 'addEmployee'
-            },
         ])
         .then((response) =>
             console.log(response),
@@ -45,19 +40,83 @@ const inquirer = require('inquirer');
 // Gather Engineer data
 
     // inquirer.prompt()
-
+    inquirer
+        .prompt([
+            {
+            type: 'input',
+            message: `What is your engineer's name?`,
+            name: 'engineerName',
+            },
+            {
+            type: 'input',
+            message: `What is your engineer's id?`,
+            name: 'engineerId',
+            },
+            {
+            type: 'input',
+            message: `What is your engineer's email?`,
+            name: 'engineerEmail',
+            },
+            {
+            type: 'input',
+            message: `What is your engineer's GitHub username?`,
+            name: 'engineerGitHub',
+            },
+        ])
+        .then((response) =>
+            console.log(response),
+            
+        )
     // THEN build an Engineer object
 
 // Gather Intern data
 
     // inquirer.prompt()
-
+    inquirer
+        .prompt([
+            {
+            type: 'input',
+            message: `What is your intern's name?`,
+            name: 'internName',
+            },
+            {
+            type: 'input',
+            message: `What is your intern's id?`,
+            name: 'internId',
+            },
+            {
+            type: 'input',
+            message: `What is your intern's email?`,
+            name: 'internEmail',
+            },
+            {
+            type: 'input',
+            message: `What is your intern's school?`,
+            name: 'internSchool',
+            },
+        ])
+        .then((response) =>
+            console.log(response),
+            
+        )
     // THEN build an Intern object
 
 // Ask "which team menmber they want to add" or "are they done"
 
     // inquirer.prompt()
-
+    inquirer
+        .prompt([
+            {
+            type: 'list',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
+            message: `Which type of team member would you like to add?`,
+            name: 'addEmployee'
+            },
+        ])
+        .then((response) =>
+            console.log(response),
+            
+        )
     // THEN deciding which function to call
 
 // Generate the HTML and write it to a file
