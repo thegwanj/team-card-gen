02acf983dbfa9fdb138f85423b3856dcab9c04c8
thegwanj <gwanjuchung@gmail.com>
2022-10-21 14:27:13 -0700

    Adding in pseudocode from demo

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // require 'inquirer'
+const inquirer = require('inquirer');
+
+// Create an empty array list to store employee objects
+
+// Gather manager data
+
+    // inquirer.prompt()
+
+    // THEN build a Manager object
+
+// Gather Engineer data
+
+    // inquirer.prompt()
+
+    // THEN build an Engineer object
+
+// Gather Intern data
+
+    // inquirer.prompt()
+
+    // THEN build an Intern object
+
+// Ask "which team menmber they want to add" or "are they done"
+
+    // inquirer.prompt()
+
+    // THEN deciding which function to call
+
+// Generate the HTML and write it to a file
