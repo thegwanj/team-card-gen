cb41cf3be8c6ebc19c8d9047617a01b6046e69f6
unknown <gwanjuchung@yahoo.com>
2022-10-22 22:50:22 -0700

    Imported objects and fixed prompts

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // require 'inquirer'
 const inquirer = require('inquirer');
+const fs = require('fs');
+
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 // Create an empty array list to store employee objects
 let employees = [];
 
 // Gather manager data
 
-    // inquirer.prompt()
+function managerPrompts(){
     inquirer
         .prompt([
             {
@@ -30,16 +36,20 @@ let employees = [];
             name: 'managerOffice',
             },
         ])
-        .then((response) =>
-            console.log(response),
-            
-        )
+        .then((response) => {
+            console.log(response);
+            // THEN build a Manager object
+            var manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerOffice);
+            employees.push(manager);
 
-    // THEN build a Manager object
+            choicePrompts();
+    })
+    }
+    
 
 // Gather Engineer data
 
-    // inquirer.prompt()
+function engineerPrompts(){
     inquirer
         .prompt([
             {
@@ -63,15 +73,18 @@ let employees = [];
             name: 'engineerGitHub',
             },
         ])
-        .then((response) =>
-            console.log(response),
+        .then((response) => {
+            console.log(response);
+            // THEN build an Engineer object
+            var engineer = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.engineerGitHub);
+            employees.push(engineer);
             
-        )
-    // THEN build an Engineer object
-
+            choicePrompts();
+        })
+    }
 // Gather Intern data
 
-    // inquirer.prompt()
+function internPrompts(){
     inquirer
         .prompt([
             {
@@ -95,15 +108,19 @@ let employees = [];
             name: 'internSchool',
             },
         ])
-        .then((response) =>
-            console.log(response),
+        .then((response) => {
+            console.log(response);
+            // THEN build an Intern object
+            var intern = new Intern(response.internName, response.internId, response.internEmail, response.internSchool);
+            employees.push(intern);
             
-        )
-    // THEN build an Intern object
+            choicePrompts();
+        })
+    }
 
 // Ask "which team menmber they want to add" or "are they done"
 
-    // inquirer.prompt()
+function choicePrompts(){
     inquirer
         .prompt([
             {
@@ -113,10 +130,55 @@ let employees = [];
             name: 'addEmployee'
             },
         ])
-        .then((response) =>
-            console.log(response),
-            
-        )
-    // THEN deciding which function to call
+        .then((response) => {
+            console.log(response);
+            // THEN deciding which function to call
+            if(response.addEmployee == 'Engineer'){
+                engineerPrompts();
+            } else if(response.addEmployee == 'Intern'){
+                internPrompts();
+            } else {
+                console.log(employees);
+                generateHTML();
+            }
+        })
+    }
 
 // Generate the HTML and write it to a file
+function generateHTML(){
+    let html = ``;
+
+    // Loop through each employee in employees
+    for(employee in employees){
+        // See what role they are
+        if(employee.getRole() == 'Manager'){
+            html += `<div>
+            <h2>${employee.name}</h2>
+            <h2>${employee.id}</h2>
+            <h2>${employee.email}</h2>
+            <h2>${employee.office}</h2>
+            </div>`;
+        }else if(employee.getRole() == 'Engineer'){
+            html += `<div>
+            <h2>${employee.name}</h2>
+            <h2>${employee.id}</h2>
+            <h2>${employee.email}</h2>
+            <a>href="https://github.com/${employee.github}">${employee.github}</a>
+            </div>`;
+        }else {
+            html += `<div>
+            <h2>${employee.name}</h2>
+            <h2>${employee.id}</h2>
+            <h2>${employee.email}</h2>
+            <h2>${employee.school}</h2>
+            </div>`;
+        }
+        // Save template to file        
+        fs.writeFile(`./output/output.html`, html, err => {
+            err ? console.error(err) : console.log('Success!')
+        });
+    }
+}
+
+
+managerPrompts();
